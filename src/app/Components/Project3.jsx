d62e291996043ef6f7f51ad2922ded024c1eac52
guptadhairy <122604403+guptadhairy@users.noreply.github.com>
2024-01-06 22:29:09 +0530

    Update Project3.jsx

--- src/app/Components/Project3.jsx
+++ src/app/Components/Project3.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
+import {BsGithub} from 'react-icons/bs'
+import Image from 'next/image';
 
-
-const Project2 = () => {
+const Project3 = () => {
   return (
     <>
-    <h1 style={{fontSize: "7rem"}}>Thank You</h1>
+    <div className='home1'>
+      <div className='left1'>
+        <video className="shiva" src="/shivVdo.mp4" autoPlay loop muted></video>
+      </div>
+      <div className='pr_right'>
+        <h1 >Temple Web-App </h1>
+        <p className='para'>This is a Temple Web-App . It was made on React. The functionality of this web-app is that you can browse recorded videos of Aarti online . You can donate online and also you can contact to the pandit ji of the temple</p>
+        <div className='show'>
+        <Link className='prButton' target='_blank' href={'https://temple-second-testing.vercel.app/'}>Visit Project </Link>
+        <Link href={'https://github.com/guptadhairy/Temple'} target='_blank'><BsGithub size={'40px'} /></Link>
+        </div>
+      </div>
+    </div>
+    
     </>
   )
 }
 
-export default Project2
+export default Project3
