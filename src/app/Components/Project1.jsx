3e7462e3d7a0be72111d0424a7f49bc94ad25c87
guptadhairy <122604403+guptadhairy@users.noreply.github.com>
2024-01-07 14:46:01 +0530

    Update Project1.jsx

--- src/app/Components/Project1.jsx
+++ src/app/Components/Project1.jsx
@@ -12,7 +12,7 @@ const Project1 = () => {
       </div>
       <div className='pr_right'>
         <h1 >Learnr </h1>
-        <p className='para'>I've developed a MERN-stack web app that streamlines student enrollment in courses, offering a smooth platform for purchasing and learning. The app features an admin dashboard for creating and managing courses, along with access to detailed financial and statistical data, including profit analytics.</p>
+        <p className='para'>I have developed a MERN-stack web app that streamlines student enrollment in courses, offering a smooth platform for purchasing and learning. The app features an admin dashboard for creating and managing courses, along with access to detailed financial and statistical data, including profit analytics.</p>
         <div className='show'>
         <Link className='prButton' target='_blank' href={'https://learnr-in.vercel.app/'}>Visit Project </Link>
         <Link href={'https://github.com/guptadhairy/Learnr'} target='_blank'><BsGithub size={'40px'} /></Link>
