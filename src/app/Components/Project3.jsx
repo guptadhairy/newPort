1d7c5fb689eb24f498d9d681672224f510b0b3a8
guptadhairy <122604403+guptadhairy@users.noreply.github.com>
2024-01-07 14:42:41 +0530

    Update Project3.jsx

--- src/app/Components/Project3.jsx
+++ src/app/Components/Project3.jsx
@@ -12,7 +12,7 @@ const Project3 = () => {
       </div>
       <div className='pr_right'>
         <h1 >Temple Web-App </h1>
-        <p className='para'>I developed a React-based Temple Web App with features that allow users to seamlessly access recorded Aarti videos online. The platform facilitates online donations and provides a streamlined communication channel to connect with the temple's priest.</p>
+        <p className='para'>This is a Temple Web-App . It was made on React. The functionality of this web-app is that you can browse recorded videos of Aarti online . You can donate online and also you can contact to the pandit ji of the temple</p>
         <div className='show'>
         <Link className='prButton' target='_blank' href={'https://temple-second-testing.vercel.app/'}>Visit Project </Link>
         <Link href={'https://github.com/guptadhairy/Temple'} target='_blank'><BsGithub size={'40px'} /></Link>
