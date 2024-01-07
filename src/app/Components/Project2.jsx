425936a79354d92bdbc9ee32f38675bf1a22ede9
guptadhairy <122604403+guptadhairy@users.noreply.github.com>
2024-01-07 14:32:22 +0530

    Update Project2.jsx

--- src/app/Components/Project2.jsx
+++ src/app/Components/Project2.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
-import {BsGithub} from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 import Image from 'next/image';
 
 const Project2 = () => {
   return (
     <>
-    <div className='home1'>
-      <div className='left1'>
-      <video src="/bit.mp4" loop autoPlay muted></video>
-      </div>
-      <div className='pr_right'>
-        <h1 > Crypto Web-App </h1>
-        <p className='para'>This is a Crypto-Currency web-app. This app was made on React and also i have used API in this web-app for fetching the real time data of any CryptoCurrency present in the market. In this app you came to know about all the COIN Exchanges app from where you can trade also.</p>
-        <div className='show'>
-        <Link className='prButton' target='_blank' href={'https://coinstrader.vercel.app'}>Visit Project </Link>
-        
+      <div className='home1'>
+        <div className='left1'>
+          <video className='shiva' src="/project2.mp4" loop autoPlay muted></video>
+        </div>
+        <div className='pr_right'>
+          <h1 > Crypto Web-App </h1>
+          <p className='para'>
+          This is a Crypto-Currency web-app. This app was made on React and also i have used API in this web-app for fetching the real time data of any CryptoCurrency present in the market. In this app you came to know about all the COIN Exchanges app from where you can trade also.</p>
+          <div className='show'>
+            <Link className='prButton' target='_blank' href={'https://coinstrader.vercel.app'}>Visit Project </Link>
+
+          </div>
         </div>
       </div>
-    </div>
-    
+
     </>
   )
 }
